1bc71026015881b121cee1d28dde0c94b597395d
amaridjoudi <amari.djoudii@gmail.com>
2021-01-01 21:27:56 +0100

    feat: improve conf

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const config = require('config');
 const app = express();
 
-const { port: PORT } = config.get('api');
+const { port: PORT, host, prefix } = config.get('api');
 
-app.get('/api/v1/ubs/account', (req, res) => {
+app.get(`${prefix}/account`, (req, res) => {
   res.send();
 });
 
 app.listen(PORT, () => {
-  console.log(`listening on: http://localhost:${PORT}`);
+  console.log(`listening on: ${host}:${PORT}`);
 });
